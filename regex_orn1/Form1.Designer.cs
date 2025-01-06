2f93c1a6c1c2efdc551a00f15679caed39e8143d
emira <emira@EMIRAY>
2025-01-06 10:10:13 +0300

    Add project files.

---
+++ regex_orn1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace regex_orn1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_islem = new System.Windows.Forms.Button();
+            this.txt_islem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_islem
+            // 
+            this.btn_islem.Location = new System.Drawing.Point(33, 202);
+            this.btn_islem.Name = "btn_islem";
+            this.btn_islem.Size = new System.Drawing.Size(75, 23);
+            this.btn_islem.TabIndex = 0;
+            this.btn_islem.Text = "İşlem";
+            this.btn_islem.UseVisualStyleBackColor = true;
+            this.btn_islem.Click += new System.EventHandler(this.btn_islem_Click);
+            // 
+            // txt_islem
+            // 
+            this.txt_islem.Location = new System.Drawing.Point(33, 85);
+            this.txt_islem.Name = "txt_islem";
+            this.txt_islem.Size = new System.Drawing.Size(100, 22);
+            this.txt_islem.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_islem);
+            this.Controls.Add(this.btn_islem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_islem;
+        private System.Windows.Forms.TextBox txt_islem;
+    }
+}
+
---
+++ regex_orn1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace regex_orn1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_islem_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txt_islem.Text, "^\\s*$"))
+            {
+                MessageBox.Show("Text Kutusu Boş");
+            }
+            else
+            {
+                MessageBox.Show("Text Kutusu Boş Değil");
+            }
+        }
+    }
+}
